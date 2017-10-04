e4c85387796c00ecf56a6c9e8b7c05d6bae0a024
Michał Cłapiński <michal.clapinski@codilime.com>
2017-10-04 12:25:49 +0200

    JW-1167 backport changes I made on code review in gerrit (#28)

--- library/cpp/sandesh.cc
+++ library/cpp/sandesh.cc
@@ -16,6 +16,7 @@
 #include <base/queue_task.h>
 #include <http/http_session.h>
 #include <io/tcp_session.h>
+#include <fstream>
 
 #include <sandesh/transport/TBufferTransports.h>
 #include <sandesh/transport/TSimpleFileTransport.h>
@@ -75,6 +76,7 @@ log4cplus::Logger Sandesh::logger_ =
 
 Sandesh::ModuleContextMap Sandesh::module_context_;
 tbb::atomic<uint32_t> Sandesh::sandesh_send_ratelimit_;
+
 const char * Sandesh::SandeshRoleToString(SandeshRole::type role) {
     switch (role) {
     case SandeshRole::Generator:
@@ -172,7 +174,7 @@ void Sandesh::RecordPort(const std::string& name, const std::string& module,
     myfifoss << module << "." << getppid() << "." << name << "_port";
     std::string myfifo = (boost::filesystem::temp_directory_path() / myfifoss.str()).string();
 
-    std::ofstream temp_file(myfifo);
+    std::ofstream temp_file(myfifo.c_str());
     if (temp_file) {
         SANDESH_LOG(INFO, "SANDESH: Write " << name << "_port " << port <<
                           "TO : " << myfifo);
